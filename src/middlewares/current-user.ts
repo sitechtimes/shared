ce9e35d6da73e1b225711121ed58cab5eecfca70
edis <edis@gonuler.com>
2021-04-05 12:06:33 -0400

    Updates

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -16,12 +16,16 @@ declare global {
 }
 
 export const currentUser = (req: Request, res: Response, next: NextFunction) => {
-    if (!req.session?.jwt) {
+    if (!req.headers.authorization) {
         return next();
     }
 
+    // if (!req.session?.jwt) {
+    //     return next();
+    // }
+
     try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
+        const payload = jwt.verify(req.headers.authorization, process.env.JWT_KEY!) as UserPayload;
         req.currentUser = payload;
     } catch (err) { }
 
